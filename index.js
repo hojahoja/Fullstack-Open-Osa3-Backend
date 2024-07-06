5bb967037806700f7e603647f9cfec129402d1c3
hojahoja <5722145+hojahoja@users.noreply.github.com>
2024-07-06 11:50:51 +0300

    3.2: puhelinluettelon backend step2

--- index.js
+++ index.js
@@ -1,48 +1,57 @@
 const express = require("express");
 const app = express();
 
-let persons = {
-  persons: [
-    {
-      name: "Jordan Rudess",
-      number: "+1-617-5681235",
-      id: "0",
-    },
-    {
-      name: "Robert Fripp",
-      number: "+44-020-2133453",
-      id: "1",
-    },
-    {
-      name: "Keith Emerson",
-      number: "+44-020-1236542",
-      id: "2",
-    },
-    {
-      name: "Neil Peart",
-      number: "+1-310-2145831",
-      id: "3",
-    },
-    {
-      name: "Tosin Abasi",
-      number: "+1-802-5324008",
-      id: "4",
-    },
-    {
-      id: "e36f",
-      name: "Heikki Silvennoinen",
-      number: "050-101012",
-    },
-    {
-      id: "38ec",
-      name: "Allan Holdsworth",
-      number: "+1-442-4321243",
-    },
-  ],
-};
+app.use(express.json());
 
-app.get("/api/persons", (request, response) => {
-  response.json(persons);
+let persons = [
+  {
+    name: "Jordan Rudess",
+    number: "+1-617-5681235",
+    id: "0",
+  },
+  {
+    name: "Robert Fripp",
+    number: "+44-020-2133453",
+    id: "1",
+  },
+  {
+    name: "Keith Emerson",
+    number: "+44-020-1236542",
+    id: "2",
+  },
+  {
+    name: "Neil Peart",
+    number: "+1-310-2145831",
+    id: "3",
+  },
+  {
+    name: "Tosin Abasi",
+    number: "+1-802-5324008",
+    id: "4",
+  },
+  {
+    id: "e36f",
+    name: "Heikki Silvennoinen",
+    number: "050-101012",
+  },
+  {
+    id: "38ec",
+    name: "Allan Holdsworth",
+    number: "+1-442-4321243",
+  },
+];
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+app.get("/api/info", (req, res) => {
+  const date = new Date();
+  const dateString = `${date.toDateString()} ${date.toTimeString()}`;
+
+  res.send(
+    `<p>The phonebook has ${persons.length} entries</p><p>${dateString}</p>`
+  );
 });
 
 const PORT = 3001;
